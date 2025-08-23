b6a5c62290b5f760e0085fe53936a1558b4817cd
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2025-08-23 21:56:31 +0530

    Add files via upload

---
+++ JavaScript/Event.js
@@ -0,0 +1,7 @@
+// This function is called when any event triggers
+function callOnEventTrigger() {
+    console.log('Hi! Event Triggered Successfully.');
+}
+
+// Call function once on page load (optional)
+callOnEventTrigger();
